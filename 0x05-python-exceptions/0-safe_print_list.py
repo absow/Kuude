fcda2d030720dc7fbb561fa3a6858389b34869be
Abdullahi Bashir <abdullahibashirsow@gmail.com>
2022-09-12 23:43:08 +0100

    First commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    try:
-        print(x)
-    except Exception:
-        if x > my_list:
-            print("The x is grater than the value")
+    total 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i], end="")
+            total +=1
+        except IndexError:
+            break
+    print("")
+    return(total)
