717c82b23beaed9147dc57d6d639de5a65024fe6
Abdullahi Bashir <abdullahibashirsow@gmail.com>
2022-09-28 03:24:41 +0100

    fix

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-def add_attribute(obj, name, value):
-    """ Function that adds a new attribute to an object
-    Args:
-        obj: object
-        name: attribute name
-        value: attribute value
-    Raises:
-        TypeError: when the attribute can't be added
-    """
+"""dds a new attribute to an object
+if it’s possible
+"""
 
-    if not hasattr(obj, "__dict__"):
+
+def add_attribute(obj, attribute, value):
+    """dds a new attribute"""
+    if '__dict__' not in dir(obj):
         raise TypeError("can't add new attribute")
-    setattr(obj, name, value)
+    if '__slots__' in dir(obj):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, attribute, value)
