977a27c00c7ca2d6682a4ec5929a8718b7857ce0
Abdullahi Bashir <abdullahibashirsow@gmail.com>
2022-11-03 12:09:35 +0100

    commiting

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Listing the name and socre of the second table 
-
-SELECT name, score FROM second_table ORDER BY score DESC;
+-- record are listed in order of descending
+SELECT `name`, `score` FROM `second_table` ORDER BY `score` DESC;
