15092a3ba3430dedf5e1a1233bb68d6037c016e6
Abdullahi Bashir <abdullahibashirsow@gmail.com>
2022-11-06 17:08:37 +0100

    first commit

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- script to show all previllages of two users
 -- Query to list all privileges (GRANT) of the MySQL users
 SHOW GRANT FOR `user_0d_1`@`localhost`;
-SHOW GRAND FOR `user_0d_2`@`loclahost`;
+SHOW GRAND FOR `user_0d_2`@`localhost`;
