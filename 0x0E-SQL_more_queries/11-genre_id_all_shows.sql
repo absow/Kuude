1406812e900951012c8c2e5d1b42d368ef57b700
Abdullahi Bashir <abdullahibashirsow@gmail.com>
2022-11-06 19:01:31 +0100

    first commit

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,8 @@
--- Lists all cities in the database hbtn_0d_usa.
--- Records are sorted in order of ascending cities.id.
-SELECT c.`id`, c.`name`, s.`name`
-	FROM `cities` AS c
-		INNER JOIN `states` AS s
-		ON c.`state_id` = s.`id`
-	ORDER BY c.`id`;
+-- Lists all shows contained in the database hbtn_0d_tvshows.
+-- Displays NULL for shows without genres.
+-- Records are ordered by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+	FROM `tv_shows` AS s
+		LEFT JOIN `tv_show_genres` AS g
+		ON s.`id` = g.`show_id`
+	ORDER BY s.`title`, g.`genre_id`;
