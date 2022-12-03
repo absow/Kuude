b34273f11799282711c7d9dbde1da32385af4a48
Abdullahi Bashir <abdullahibashirsow@gmail.com>
2022-12-03 09:49:57 +0100

    my commit

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
-#!/usr/bin/bash
+#!/usr/bin/node
 const multi = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for(let i = 0; i < multi.length; i++){
+for (let i = 0; i < multi.length; i++) {
 	console.log(multi[i]);
 }
