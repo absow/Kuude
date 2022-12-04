2276892f853405a8bb932fbfabb357bfbf03b667
Abdullahi Bashir <abdullahibashirsow@gmail.com>
2022-12-04 15:16:23 +0100

    commti

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 function factorial (num) {
-	return num === 0 || isNaN(num) ? 1 : num * factorial(num -1);
+  return num === 0 || isNaN(num) ? 1 : num * factorial(num - 1);
 }
 
 const number = parseInt(process.argv[2]);
